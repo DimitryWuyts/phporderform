8cc5df10b721d7e0c3f61f53e132460b8139b7c6
dimitry <dimiw1337@hotmail.com>
2020-02-11 05:29:01 -0500

    tried to fix adress, works +-

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php
-//this line makes PHP behave in a more strict way
+
+//strict type
 declare(strict_types=1);
+//enable errors
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
-//we are going to use session variables so we need to enable sessions
+
+//enable sessions
 session_start();
 
 function whatIsHappening() {
@@ -17,8 +20,9 @@ function whatIsHappening() {
     echo '<h2>$_SESSION</h2>';
     var_dump($_SESSION);
 }
+$totalValue = 0;
 
-//your products with their price.
+//products with price
 if(isset($_GET["food"]) && $_GET["food"] == 0){
     $products = [
         ['name' => 'Cola', 'price' => 2],
@@ -42,6 +46,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 //email
     $email = input($_POST["email"]);
+
 //if empty, alert
     if (empty($email)) {
         $alerts[] = "Please fill in your <a href='#email' class='alert-link'>E-mail</a>!";
@@ -51,10 +56,35 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $errors[] = "<a href='#email' class='alert-link'>$email</a> is not a valid email address!";
         }
     }
+
+//address
+    $street = input($_POST["street"]);
+    $streetNumber = input($_POST["streetnumber"]);
+    $city = input($_POST["city"]);
+    $zipCode = input($_POST["zipcode"]);
+
+//making sure address is valid
+    if (empty($street)) {
+        $alerts[] = "Please fill in your <a href='#street' class='alert-link'>street</a>!";
+    }
+    if (empty($streetnumber)) {
+        $alerts[] = "Please fill in your <a href='#streetnumber' class='alert-link'>street number</a>!";
+    } else {
+        if (!is_numeric($streetnumber)) {
+            $errors[] = "<a href='#streetnumber' class='alert-link'>Street Number</a> only accepts numbers!";
+        }
+        if (empty($city)) {
+            $alerts[] = "Please fill in your <a href='#city' class='alert-link'>city</a>!";
+        }
+
+        if (empty($zipcode)) {
+            $alerts[] = "Please fill in your <a href='#zipcode' class='alert-link'>zipcode</a>!";
+        } else {
+            if (!is_numeric($zipcode)) {
+                $errors[] = "<a href ='#zipcode' class='alert-link'>Zipcode</a> only accepts numbers!";
+            }
+        }
+    }
 }
-$totalValue = 0;
-
- require 'form-view.php';
-
-?>
+require 'form-view.php';
 
